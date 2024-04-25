e5b8f7ba986a4b5a9a702922880309c09a5b8e88
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-04-25 20:27:44 +0100

    Update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,29 +6,23 @@ jest.spyOn(global.console, 'log').mockImplementation((text) => {
     loggedString = text;
 });
 
+const inputs = [
+    ['test input', 'test input'],
+    ['Woof woof, bark bark!', 'Woof woof, bark bark!'],
+    [6, '6'],
+    [234, '234'],
+    [1823.209, '1823.209'],
+    [null, 'null'],
+    [['foo', 'bar'], '["foo","bar"]'],
+    [{ woof: 'bark' }, '{"woof":"bark"}'],
+    [undefined, DEFAULT_WOOF],
+];
+
 test('console.dog is a defined function', () => {
     expect(typeof console.dog).toBe('function');
 });
 
-it('Dogs defined inputs', () => {
-    const inputs = [
-        'test input',
-        'Woof woof, bark bark!',
-        6,
-        234,
-        1823.209,
-        null,
-        ['foo', 'bar'],
-        { woof: 'bark' },
-    ];
-
-    inputs.forEach((input) => {
-        console.dog(input);
-        expect(loggedString).toBe([BORDER, input?.toString(), BORDER, ...DOG].join('\n'));
-    });
-});
-
-it('Dogs a default response with an undefined', () => {
-    console.dog();
-    expect(loggedString).toBe([BORDER, DEFAULT_WOOF, BORDER, ...DOG].join('\n'));
+test.each(inputs)('Logs an input of %s as %s', (input, output) => {
+    console.dog(input);
+    expect(loggedString).toBe([BORDER, output, BORDER, ...DOG].join('\n'));
 });
